4a69b180f6feecc57ae2cdf3e90d2686fff48bb2
Pablo López-García <plopezgarcia@gmail.com>
2016-07-01 16:19:27 +0200

    completed example

--- src/test/java/at/medunigraz/imi/bst/GeneralTest.java
+++ src/test/java/at/medunigraz/imi/bst/GeneralTest.java
@@ -12,8 +12,10 @@ public class GeneralTest {
         // Gets all patterns for CF subhierarchy, in descending order, which at least appear 1000 times
 
         List<PatternFrequency> aListOfFreqs = TopLevelConcept.getPatternFrequency(TopLevelConcept.SUBHIERARCHY.CF, 6000);
-
         System.out.println(aListOfFreqs);
+
+        System.out.println("The most frequent pattern for Clinical Finding is: " + aListOfFreqs.get(0).pattern);
+        System.out.println("In SNOMED CT, its used by: " + aListOfFreqs.get(0).frequency + " concepts");
     }
 
 }
