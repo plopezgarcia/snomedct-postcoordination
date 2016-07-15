2502c96e7b119eaad68517bbf57cee699437ffdb
joseminya <jose.minya@gmail.com>
2016-07-15 12:34:13 +0200

    Change integers into strings to use large ids for SCT

--- src/main/java/at/medunigraz/imi/bst/TopLevelConcept.java
+++ src/main/java/at/medunigraz/imi/bst/TopLevelConcept.java
@@ -27,32 +27,32 @@ public final class TopLevelConcept {
     }
 
     public enum SUBHIERARCHY {  BS, CF, EG, EV, OE, OR, PB, PF, PO,
-                                PR, QV, RA, SI, SO, SP, SN, ST, SU}
+    							PR, QV, RA, SI, SO, SP, SN, ST, SU, SM}
 
 
     // Probably useless... here anyway for now
-    public static final Map<SUBHIERARCHY, Integer> SUBHIERARCHY_MAPPINGS;
+    public static final Map<SUBHIERARCHY, String> SUBHIERARCHY_MAPPINGS;
     static {
-        Map<SUBHIERARCHY, Integer> theMap = new HashMap<SUBHIERARCHY, Integer>();
-        theMap.put(SUBHIERARCHY.BS, 123037004);
-        theMap.put(SUBHIERARCHY.CF, 404684003);
-        theMap.put(SUBHIERARCHY.EG, 308916002);
-        theMap.put(SUBHIERARCHY.EV, 272379006);
-        theMap.put(SUBHIERARCHY.OE, 363787002);
-        theMap.put(SUBHIERARCHY.OR, 410607006);
-        theMap.put(SUBHIERARCHY.PB, 373873005);
-        theMap.put(SUBHIERARCHY.PF, 78621006);
-        theMap.put(SUBHIERARCHY.PO, 260787004);
-        theMap.put(SUBHIERARCHY.PR, 71388002);
-        theMap.put(SUBHIERARCHY.QV, 362981000);
-        theMap.put(SUBHIERARCHY.RA, 419891008);
-        theMap.put(SUBHIERARCHY.SI, 243796009);
-        theMap.put(SUBHIERARCHY.SO, 48176007);
-        theMap.put(SUBHIERARCHY.SP, 370115009);
-        theMap.put(SUBHIERARCHY.SN, 123038009);
-        theMap.put(SUBHIERARCHY.ST, 254291000);
-        theMap.put(SUBHIERARCHY.SU, 105590001);
-        SUBHIERARCHY_MAPPINGS = Collections.unmodifiableMap(theMap);
+    	Map<SUBHIERARCHY, String> theMap = new HashMap<SUBHIERARCHY, String>();
+    	theMap.put(SUBHIERARCHY.BS, "123037004");
+    	theMap.put(SUBHIERARCHY.CF, "404684003");
+    	theMap.put(SUBHIERARCHY.EG, "308916002");
+    	theMap.put(SUBHIERARCHY.EV, "272379006");
+    	theMap.put(SUBHIERARCHY.OE, "363787002");
+    	theMap.put(SUBHIERARCHY.OR, "410607006");
+    	theMap.put(SUBHIERARCHY.PB, "373873005");
+    	theMap.put(SUBHIERARCHY.PF, "78621006");
+    	theMap.put(SUBHIERARCHY.PO, "260787004");
+    	theMap.put(SUBHIERARCHY.PR, "71388002");
+    	theMap.put(SUBHIERARCHY.QV, "362981000");
+    	theMap.put(SUBHIERARCHY.RA, "419891008");
+    	theMap.put(SUBHIERARCHY.SI, "243796009");
+    	theMap.put(SUBHIERARCHY.SO, "48176007");
+    	theMap.put(SUBHIERARCHY.SP, "370115009");
+    	theMap.put(SUBHIERARCHY.SN, "123038009");
+    	theMap.put(SUBHIERARCHY.ST, "254291000");
+    	theMap.put(SUBHIERARCHY.SU, "105590001");
+    	theMap.put(SUBHIERARCHY.SM, "900000000000441003");
+    	SUBHIERARCHY_MAPPINGS = Collections.unmodifiableMap(theMap);
     }
-
 }
